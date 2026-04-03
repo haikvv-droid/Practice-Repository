2730672626898560455dac4cb329446014178e4e
Menis Dukhin <ownagefish@gmail.com>
2026-04-03 01:02:47 -0400

    Move x to 5.

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ if x == 5:
     print("x is 5")
 if x != 5:
     print("x is not 5") 
+    while x != 5:
+        if x > 5:
+            x-=1
+        else:
+            x+=1
+        print("x is now ", x)
